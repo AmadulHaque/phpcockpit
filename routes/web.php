10d7af07207e0ff0cf56d51eb59bd055f5b200bd
AmadulHaque <mdamadulhaque8@gmail.com>
2026-03-04 23:00:59 +0600

    ok

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Livewire\Projects\Index;
-use App\Livewire\Projects\Show;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', Index::class)->name('projects.index');
-Route::get('/projects/{project}', Show::class)->name('projects.show');
